4112748ea7e9bb38c03fd09061d684ea2826ce67
aaswimacademy26 <aaswimacademy26@gmail.com>
2026-03-22 20:22:20 -0400

    Modify Vite config for GitHub Pages
    Updated Vite configuration for GitHub Pages deployment and removed Netlify plugin.

--- source-69c07717fd19e2262f8ece55-fe21b9d88a7925d3/vite.config.ts
+++ source-69c07717fd19e2262f8ece55-fe21b9d88a7925d3/vite.config.ts
@@ -3,15 +3,15 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
 
+// Replace '<repo-name>' with your GitHub repository name
 const config = defineConfig({
+  base: '/<repo-name>/', // important for GitHub Pages
   plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    netlify(),
     tanstackStart(),
     viteReact(),
   ],
