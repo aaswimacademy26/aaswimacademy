ac0313bfd27f8f71ac48423a6450759c7400f062
aaswimacademy26 <aaswimacademy26@gmail.com>
2026-03-22 20:22:45 -0400

    Change base URL in vite.config.ts
    Update base URL for GitHub Pages in Vite config

--- source-69c07717fd19e2262f8ece55-fe21b9d88a7925d3/vite.config.ts
+++ source-69c07717fd19e2262f8ece55-fe21b9d88a7925d3/vite.config.ts
@@ -4,9 +4,8 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
-// Replace '<repo-name>' with your GitHub repository name
 const config = defineConfig({
-  base: '/<repo-name>/', // important for GitHub Pages
+  base: '/aaswimacademy/', // important for GitHub Pages
   plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
